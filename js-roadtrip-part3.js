5c857d4f30f46137e413d0a7d12cfeece6f337bf
Robert Fauver <robertfauver@gmail.com>
2014-01-23 13:03:58 -0500

    Create js-roadtrip-part3.js

---
+++ js-roadtrip-part3.js
@@ -0,0 +1,25 @@
+//JS Roadtrip Part3
+
+//1
+var fear = fearGenerated(numPeeps, rainInInches, numSharks);
+var fearMessage;
+
+var low = function() {
+};
+
+var medium = function() {
+};
+var high = function() {
+};
+
+var fearMessage = function() {
+};
+
+confirmRide(fearMessage);
+
+function confirmRide( confirmToGo ){
+  return confirmToGo();
+}
+
+var startRide = function() {
+};
