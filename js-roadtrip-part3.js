f357d06dacc6fc0e88726e8c11d05c840f529738
Robert Fauver <robertfauver@gmail.com>
2014-01-23 13:18:26 -0500

    Update js-roadtrip-part3.js

--- js-roadtrip-part3.js
+++ js-roadtrip-part3.js
@@ -4,12 +4,19 @@
 var fear = fearGenerated(numPeeps, rainInInches, numSharks);
 var fearMessage;
 
+//For fear levels less than 200
 var low = function() {
+  prompt("Fear Level: LOW Should be no problem at all...mwahaha. Still wanna ride?");
 };
 
+//For fear levels 200 through and including 300
 var medium = function() {
+  prompt("Fear Level: MEDIUM. You may want to rethink this one, man. MWAHAHA! Think you'll make it?");
 };
+
+//For fear levels above 300 and up to 400
 var high = function() {
+  prompt("Fear Level: HIGH ABANDON ALL HOPE!! Have a death wish?");
 };
 
 var fearMessage = function() {
