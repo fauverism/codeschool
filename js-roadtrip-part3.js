d2cae697a4bb9b3cf0ce2deafad26de2245f5a11
fauverism <robertfauver@gmail.com>
2014-03-29 20:25:24 -0400

    just working thru the exams

--- js-roadtrip-part3.js
+++ js-roadtrip-part3.js
@@ -1,30 +1,106 @@
-//JS Roadtrip Part3
-
-//1
-
-//1.5
-var fear = fearGenerated(numPeeps, rainInInches, numSharks);
-var fearMessage = function() {
-
-};
-
-//For fear levels less than 200
-var low = function() {
-  prompt("Fear Level: LOW Should be no problem at all...mwahaha. Still wanna ride?");
-};
-
-//For fear levels 200 through and including 300
-var medium = function() {
-  prompt("Fear Level: MEDIUM. You may want to rethink this one, man. MWAHAHA! Think you'll make it?");
-};
-
-//For fear levels above 300 and up to 400
-var high = function() {
-  prompt("Fear Level: HIGH ABANDON ALL HOPE!! Have a death wish?");
-};
-
-function confirmRide( confirmToGo, fearMessage ){
-  return confirmToGo();
+function theBridgeOfHoistingDoom() {
+    var ring = undefined;
+    var power = undefined;
+    var wizard = undefined;
+    function balrog() {
+      return "fire";
+    }
+    function wizard(){
+        return "white";
+    }
+    function elf() {
+        return "immortal";
+    }
 }
 
-var startRide = confirmRide();
+//4.2
+var vehicle1 = { type: "Motorboat", capacity: 6, storedAt: "Ammunition Depot" };
+var vehicle2 = { type: "Jet Ski", capacity: 1, storedAt: "Reef Dock" };
+var vehicle3 = { type: "Submarine", capacity: 8, storedAt: "Underwater Outpost" };
+
+//4.4
+var vehicle1 = { type: "Motorboat", capacity: 6, storedAt: "Ammunition Depot" };
+var vehicle2 = { type: "Jet Ski", capacity: 1, storedAt: "Reef Dock" };
+var vehicle3 = { type: "Submarine", capacity: 8, storedAt: "Underwater Outpost" };
+
+var vehicles = [vehicle1,vehicle2,vehicle3];
+
+var findVehicle = function(name, list) {
+    for (var i = 0; i<list.length; i++) {
+        if(list[i].type === name) {
+            return list[i].storedAt;
+        }
+    }
+};
+findVehicle("Submarine", vehicles);
+
+//4.5
+var vehicle1 = {type: "Motorboat", capacity: 6, storedAt: "Ammunition Depot"};
+var vehicle2 = {type: "Jet Ski", capacity: 1, storedAt: "Reef Dock"};
+var vehicle3 = {type: "Submarine", capacity: 8, storedAt: "Underwater Outpost"};
+
+vehicle1.capacity(10);
+vehicle2.submersible(false);
+vehicle3.weapon("Torpedoes");
+vehicle1.submersible(false);
+vehicle2.weapon("Lasers”);
+vehicle3.capacity(16);
+vehicle1.weapon("Rear-Mounted Slingshot");
+vehicle3.submersible(true);
+
+//4.6
+var vehicle1 = {type: "Motorboat", capacity: 6, storedAt: "Ammunition Depot"};
+var vehicle2 = {type: "Jet Ski", capacity: 1, storedAt: "Reef Dock"};
+var vehicle3 = {type: "Submarine", capacity: 8, storedAt: "Underwater Outpost"};
+vehicle1.capacity += 4;
+vehicle2.submersible = false;
+vehicle3.weapon = "Torpedoes";
+vehicle1.submersible = false;
+vehicle2.weapon = "Lasers";
+vehicle3.capacity *= 2;
+vehicle1.weapon = "Rear-Mounted Slingshot";
+vehicle3.submersible = true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
